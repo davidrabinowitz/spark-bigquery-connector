40ca8b35f2fffe350104c29e2cdda2e0560d0338
David Rabinowitz <davidrab@google.com>
2026-06-16 10:06:03 -0700

    cleanup

--- patch_bigqueryutil.sh
+++
@@ -1,7 +0,0 @@
-sed -i '' -e '/import com.google.common.base.Preconditions;/a\
-import java.lang.reflect.Field;\
-import java.util.ArrayList;\
-import java.util.List;\
-import org.slf4j.Logger;\
-import org.slf4j.LoggerFactory;\
-' bigquery-connector-common/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryUtil.java
